2c3a7db4730718b7e72aa5855dab40afebb43742
tuanstvn1215 <39292548+tuanstvn1215@users.noreply.github.com>
2023-02-06 10:48:14 +0700

    .

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+const port = process.env.port || 3000
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/', (req, res) => {
+  console.log(req)
+  res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
